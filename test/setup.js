7f357cacb06fc850d2d114f7c6fdfb7cb4baec86
Victoria Moore <victoriamoore563@gmail.com>
2020-02-01 14:09:42 -0800

    added JWT secret

--- test/setup.js
+++ test/setup.js
@@ -1,10 +1,14 @@
 process.env.TZ = "UCT";
 process.env.NODE_ENV = "test";
+process.env.JWT_SECRET = "test-jwt-secret"
 require("dotenv").config();
+
 process.env.TEST_DB_URL =
-  process.env.TEST_DB_URL ||
-  "postgresql://dunder_mifflin@localhost/thingful-test";
+process.env.TEST_DB_URL ||
+"postgresql://dunder_mifflin@localhost/thingful-test";
+
 const { expect } = require("chai");
 const supertest = require("supertest");
+
 global.expect = expect;
 global.supertest = supertest;
