5f63abcb7964f6920486f514dc6fe006925ef0c2
Victoria Moore <victoriamoore563@gmail.com>
2020-02-02 15:31:48 -0800

    minor changes made throughout

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,15 +1,15 @@
 const AuthService = require('../auth/auth-service')
 
 function requireAuth(req, res, next) {
-    const authToken = req.get('Authorization') || ''
+    const authToken = req.get('authorization') || ''
     let basicToken 
 
-    if(!authToken.toLowerCase().startsWith('basic ')) {
+    if(!authToken.toLowerCase().startsWith('bearer ')) {
         return res.status(401).json({
-            error : 'Missing basic token'
+            error : 'Missing bearer token'
         })
     } else {
-        basicToken = authToken.slice('basic '.length, authToken.length)
+        basicToken = authToken.slice('bearer '.length, authToken.length)
     }
     const [ tokenUserName, tokenPassword ] = AuthService.parseBasicToken(basicToken)
 
@@ -33,7 +33,7 @@ function requireAuth(req, res, next) {
         return AuthService.comparePasswords(tokenPassword, user.password)
             .then(passwordMatch => {
                 if(!passwordMatch){
-                    return res.status(401).json( {error: 'unauthorized request'} )
+                    return res.status(401).json( {error: `Unauthorized request`} )
                 }
                 req.user =user
                 next()
